db160e73d02702eb01a8a30e6d66ac2b5fae44d1
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2022-12-19 11:50:44 +0100

    scetch for updateGeneration method done

--- src/main/java/com/example/conways_game_of_life/Grid.java
+++ src/main/java/com/example/conways_game_of_life/Grid.java
@@ -1,4 +1,27 @@
 package com.example.conways_game_of_life;
 
+import java.util.ArrayList;
+
 public class Grid {
+    // This method replaces the current generation with its successor.
+    public void updateGeneration(){
+        // Iterate over all tiles
+        for(tile: grid){
+            ArrayList<Tile> neighbors = getAliveNeighbours(i, j);
+            tile.setColor(getWinningColor(neighbors));
+        }
+        // Update the drawn grid on the GUI.
+        draw();
+    }
+
+    // Returns an ArrayList of the alive neighbors.
+    public ArrayList<Tile> getAliveNeighbours(int i, int j){
+
+    }
+
+    /* This method takes the list of neighbours of a tile.
+    * It compares the colors of the neighbours and
+    * returns the color that should be used to color the tile. */
+    private Color getWinningColor(ArrayList<Tile> neighbours){
+    }
 }
