33cc142c40a1acf0c32b3b58abee00d471fced66
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2022-12-19 14:38:46 +0100

    Helper methods for updateGeneration method

--- src/main/java/com/example/conways_game_of_life/Grid.java
+++ src/main/java/com/example/conways_game_of_life/Grid.java
@@ -47,6 +47,33 @@ public class Grid {
     * It compares the colors of the neighbours and
     * returns the color that should be used to color the tile. */
     private Color getWinningColor(ArrayList<Tile> neighbours){
-        return Color.RED;
+        // TODO Maybe put this color finding code in separate method.
+        Color colorOne; // Store one color in this field. TODO Maybe make an instance variable for this.
+        Color colorTwo; // Store the other color in this field. TODO Maybe make an instance variable for this.
+        int i = 0;
+        while(!colorOne || !colorTwo){
+            if(neighbours.get(i).isAlive() && !colorOne){
+                colorOne = neighbours.get(i).getColor();
+            } else if (neighbours.get(i).isAlive()) {
+                colorTwo = neighbours.get(i).getColor();
+            }
+        }
+
+        // Find the winning color
+        int countColorOne = 0;
+        int countColorTwo = 0;
+        for(i = 0; i < neighbours.size(); i++){
+            // TODO Is this a valid usage of switch or is this a SWITCH anti pattern?
+            switch(neighbours.get(i).getColor()){
+                case colorOne -> countColorOne++;
+                case colorTwo -> countColorTwo++;
+            }
+        }
+        // TODO Look at Andres rules if this is the correct behavior.
+        if(countColorOne > countColorTwo) {
+            return colorOne;
+        }else{
+            return colorTwo;
+        }
     }
 }
