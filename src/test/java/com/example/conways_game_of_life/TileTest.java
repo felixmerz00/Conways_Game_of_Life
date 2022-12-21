62417fa6b4b2ddd69fadc0ba01ccfc6466d9fcfb
Mely <95911782+Meleac@users.noreply.github.com>
2022-12-21 17:04:39 +0100

    implement test classes für grid and tile

--- src/test/java/com/example/conways_game_of_life/TileTest.java
+++ src/test/java/com/example/conways_game_of_life/TileTest.java
@@ -3,6 +3,8 @@ package com.example.conways_game_of_life;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,11 +12,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class TileTest {
 
     Random random = new Random();
-    Coordinate coordinate = new Coordinate(random.nextInt(18), random.nextInt(18));
     Grid grid = new Grid();
 
-    //check again because method is private (in Grid class)
-    //Tile tile = grid.getCoordinateTile(coordinate);
+    TileTest() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+    }
+
+    //make method public
+    private Tile getCoordinateTile(Coordinate coordinate) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = Grid.class.getDeclaredMethod("getCoordinateTile");
+        method.setAccessible(true);
+        return (Tile) method.invoke(coordinate);
+    }
+    Coordinate coordinate = new Coordinate(random.nextInt(18), random.nextInt(18));
+    Tile tile = getCoordinateTile(coordinate);
     Player player = new Player("TestPlayer", Color.MAGENTA);
 
     @Test
@@ -22,5 +32,42 @@ class TileTest {
         boolean out = tile.tileToPlayer(player);
         //because Tile has Color WHITE and Player MAGENTA, out should be false
         assertFalse(out);
+    }
+
+    @Test
+    void testSetAlive() {
+        boolean before = tile.isAlive();
+        tile.setAlive(!before);
+        boolean after = tile.isAlive();
+        assertFalse(before == after);
+    }
+
+    @Test
+    void testIsAlive() {
+        tile.setAlive(true);
+        assertTrue(tile.isAlive());
+
+        tile.setAlive(false);
+        assertFalse(tile.isAlive());
+    }
+
+    @Test
+    void testSetColor() {
+        tile.setColor(Color.GREEN);
+        assertTrue(tile.getColor() == Color.GREEN);
+    }
+
+    //maybe check if class of returned value is Color
+    @Test
+    void testGetColor() {}
+
+    @Test
+    void testTileToPlayer() {
+        Color tileColor = tile.getColor();
+        Color playerColor = player.getPlayerColor();
+        if (tileColor.equals(playerColor)) {
+            assertTrue(tile.tileToPlayer(player));
+        }
+        else {assertFalse(tile.tileToPlayer(player));}
     }
 }
