d21e3b9c91c49b855eabee2b12be8fc19cef2248
Mely <95911782+Meleac@users.noreply.github.com>
2022-12-21 14:19:38 +0100

    changes in Tile Class

--- src/main/java/com/example/conways_game_of_life/Color.java
+++ src/main/java/com/example/conways_game_of_life/Color.java
@@ -1,5 +1,6 @@
 package com.example.conways_game_of_life;
 
 public enum Color {
-    YELLOW, AMBER, ORANGE, VERMILLION, RED, MAGENTA, PURPLE, VIOLET, BLUE, TEAL, GREEN, CHARTREUSE
+    //Color WHITE when Tile is not used
+    WHITE, YELLOW, AMBER, ORANGE, VERMILLION, RED, MAGENTA, PURPLE, VIOLET, BLUE, TEAL, GREEN, CHARTREUSE
 }
--- src/main/java/com/example/conways_game_of_life/Tile.java
+++ src/main/java/com/example/conways_game_of_life/Tile.java
@@ -1,7 +1,22 @@
 package com.example.conways_game_of_life;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
 public class Tile {
+    //Tile Status (dead or alive)
+    private boolean alive;
+
+    //Tile Color:
+    private Color aColor;
+
+    //Constructor used when initializing Grid; set bool alive to true and Color to white
+    public Tile() {
+        alive = true;
+        aColor = Color.WHITE;
+    }
+
+    public Color getColor() {return aColor;}
+
+    public void setColor(Color c) {
+        aColor = c;
+    }
+
 }
