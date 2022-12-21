2669987e0d6769f8ddee26016c10c4335acd588b
TimoTietje <timolennard.tietje@uzh.ch>
2022-12-21 11:27:12 +0100

    InterfaceUI

---
+++ src/main/java/com/example/conways_game_of_life/InterfaceUI.java
@@ -0,0 +1,10 @@
+package com.example.conways_game_of_life;
+
+public interface InterfaceUI {
+
+    String setPlayerName();
+    String setPlayerColor();
+    void deleteTile(Player aPlayer);
+    void setTile(Player aPlayer);
+
+}
