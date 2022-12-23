85acac30b3bbe067bbfcf32975e821eaf72e858c
TimoTietje <timolennard.tietje@uzh.ch>
2022-12-23 09:29:44 +0100

    added declare Winner method and changed setPlayerColor to return a Color and not a string

--- src/main/java/com/example/conways_game_of_life/InterfaceUI.java
+++ src/main/java/com/example/conways_game_of_life/InterfaceUI.java
@@ -3,8 +3,10 @@ package com.example.conways_game_of_life;
 public interface InterfaceUI {
 
     String setPlayerName();
-    String setPlayerColor();
+    Color setPlayerColor();
     Coordinate deleteTile(Player aPlayer);
     Coordinate setTile(Player aPlayer);
+
+    void declareWinner(Player aPlayer);
 
 }
