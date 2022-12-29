93e4e084cc4a5feef082fc25682f48e415030754
sugar-free55 <andre.seidenglanz@gmail.com>
2022-12-29 19:16:05 +0100

    New colors with different starting letters

--- src/main/java/com/example/conways_game_of_life/Color.java
+++ src/main/java/com/example/conways_game_of_life/Color.java
@@ -3,5 +3,6 @@ package com.example.conways_game_of_life;
 public enum Color {
     // The following colors correspond to existing colors from the javafx.scene.paint.Class class.
     // We use the color WHITE for dead tiles.
-    YELLOW, ORANGE, ORANGERED, RED, MAGENTA, PURPLE, VIOLET, BLUE, TEAL, GREEN, CHARTREUSE, WHITE
+    YELLOW, ORANGE, RED, MAGENTA, PURPLE, VIOLET, BLUE, TEAL, GREEN, WHITE
+
 }
