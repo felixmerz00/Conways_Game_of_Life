92f89fc4a8bf1ce71739b370550c9dc523f52c61
sugar-free55 <andre.seidenglanz@gmail.com>
2022-12-29 19:14:41 +0100

    Method kill and set Method update Grid for loop adjustments

--- src/main/java/com/example/conways_game_of_life/UserInteraction.java
+++ src/main/java/com/example/conways_game_of_life/UserInteraction.java
@@ -13,7 +13,8 @@ public class UserInteraction implements InterfaceUI {
     @Override
     public String setPlayerName(int player) {
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter username for player " + player+1 + ": ");
+        System.out.println("\n-----------------------------------------");
+        System.out.println("Enter username for player " + player + ": ");
         String playerName = null;
         boolean validInput = false;
         while (!validInput) {
@@ -25,12 +26,8 @@ public class UserInteraction implements InterfaceUI {
                 System.out.println("Name must be at least on character long, try again: ");
             }
         }
-        System.out.println("Username of player "+ player+1 +" is: " + playerName +"\n");  // Output user input
+        System.out.println("Username of player "+ player +" is: " + playerName +"\n");  // Output user input
         return playerName;
-
-
-        // check if name is valid
-        // can be done much easier in GUI
     }
 
     @Override
@@ -39,7 +36,7 @@ public class UserInteraction implements InterfaceUI {
         // aks user about color and return
         Scanner aScanner = new Scanner(System.in);  // Create a Scanner object
 
-        System.out.println("Choose a color (Enter letter in brackets): ");
+        System.out.println("Choose a color for player " + player + " (Enter letter in brackets): ");
         System.out.println("Yellow (0), Orange (1), Red (2), Magenta (3), Purple (4), Violet (5), Blue (6), Teal (7), Green (8)");
 
         ArrayList<String> validInputs = new ArrayList<>(Arrays.asList("YELLOW", "ORANGE", "RED", "MAGENTA", "PURPLE", "VIOLET", "BLUE", "TEAL", "GREEN"));
@@ -58,21 +55,107 @@ public class UserInteraction implements InterfaceUI {
                 System.out.println("Yellow (0), Orange (1), Red (2), Magenta (3), Purple (4), Violet (5), Blue (6), Teal (7), Green (8)");
             }
         }
+
+        System.out.println("Color of player " + player + " is " + Color.valueOf(validInputs.get(userSelection)).toString().toLowerCase());
         return Color.valueOf(validInputs.get(userSelection));
         // TODO Make sure that not both players can choose the same color.
     }
 
     @Override
     public Coordinate deleteTile(Player aPlayer) {
-        // player not needed if:
-        // maybe do this with new GUI in the GameLogic/Grid class
-        return null;
+        Scanner aScanner = new Scanner(System.in);  // Create a Scanner object
+
+        // ask for X-Coordinate
+        System.out.println("-----------------------------------------");
+        System.out.println("\n" + aPlayer.getName() + ":");
+        System.out.println("Please enter X-Coordinate of enemy tile which you would like to kill.\n" +
+                "Enter an integer below (between 1 and 16):");
+        int userinputX = 0;
+        boolean validInput = false;
+        while (!validInput) {
+            try {
+                userinputX = Integer.parseInt(aScanner.nextLine());  // Read user input
+                if (userinputX > 0 && userinputX < 17) {
+                    validInput = true;
+                } else {
+                    System.out.println("X-Coordinate must be between 1 and 16.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter only an integer between 1 and 16.");
+            }
+        }
+        System.out.println("Your chosen X-Coordinate is " + userinputX);
+
+        // aks for Y-Coordinate
+        System.out.println("Please enter Y-Coordinate of enemy tile which you would like to kill.\n" +
+                "Enter an integer below (between 1 and 16):");
+        int userinputY = 0;
+        validInput = false;
+        while (!validInput) {
+            try {
+                userinputY = Integer.parseInt(aScanner.nextLine());  // Read user input
+                if (userinputY > 0 && userinputY < 17) {
+                    validInput = true;
+                } else {
+                    System.out.println("Y-Coordinate must be between 1 and 16.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter only an integer between 1 and 16.");
+            }
+        }
+        System.out.println("Your chosen Coordinate is (" + userinputX + "," + userinputY + ")");
+
+
+
+        // make Coordinate from user inputs
+        return new Coordinate(userinputX, userinputY);
     }
 
     @Override
     public Coordinate setTile(Player aPlayer) {
-        //
-        return null;
+        Scanner aScanner = new Scanner(System.in);  // Create a Scanner object
+
+        // ask for X-Coordinate
+        System.out.println("\n" + aPlayer.getName() + ":");
+        System.out.println("Please enter X-Coordinate of dead Tile which you would like to occupy.\n" +
+                "Enter an integer below (between 1 and 16):");
+        int userinputX = 0;
+        boolean validInput = false;
+        while (!validInput) {
+            try {
+                userinputX = Integer.parseInt(aScanner.nextLine());  // Read user input
+                if (userinputX > 0 && userinputX < 17) {
+                    validInput = true;
+                } else {
+                    System.out.println("X-Coordinate must be between 1 and 16.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter only an integer between 1 and 16.");
+            }
+        }
+        System.out.println("Your chosen X-Coordinate is " + userinputX);
+
+        // aks for Y-Coordinate
+        System.out.println("Please enter Y-Coordinate of dead Tile which you would like to occupy.\n" +
+                "Enter an integer below (between 1 and 16):");
+        int userinputY = 0;
+        validInput = false;
+        while (!validInput) {
+            try {
+                userinputY = Integer.parseInt(aScanner.nextLine());  // Read user input
+                if (userinputY > 0 && userinputY < 17) {
+                    validInput = true;
+                } else {
+                    System.out.println("Y-Coordinate must be between 1 and 16.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter only an integer between 1 and 16.");
+            }
+        }
+        System.out.println("Your chosen Coordinate is (" + userinputX + "," + userinputY + ")");
+
+        // make Coordinate from user inputs
+        return new Coordinate(userinputX, userinputY);
     }
 
     // Does only open a new window which states winner and closes stage on "Enter"
@@ -83,12 +166,32 @@ public class UserInteraction implements InterfaceUI {
 
     }
 
-
     @Override
     public void UpdateGrid(Grid aGrid){
         // print out new grid
+        System.out.println("================ NEW GENERATION ================");
+        System.out.println(" 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 ");
+        System.out.println("+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+ ");
 
+        int i = 0;
+        int j = 0;
+        for(Tile t: aGrid){
+            System.out.print(i+1);
+            while(j < 16){
+                System.out.print('|');
+                Character uppercaseLetter = t.getColor().toString().charAt(0);
+                System.out.print(uppercaseLetter+uppercaseLetter);
+                System.out.print("|");
+                j += 1;
+            }
+            System.out.print(i+1);
+            System.out.print("\n");
+            i += 1;
+        }
 
-
+        System.out.println("+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+ ");
+        System.out.println(" 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 ");
+        System.out.print("=================================================");
+        System.out.print("\n\n--------------------------------------------------\n\n");
     }
 }
