205e27f30d64f0e815b0d0d18b93b8abcc2a68f4
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2022-12-30 09:14:45 +0100

    created toString method, rearranged the colors to improve printing

--- src/main/java/com/example/conways_game_of_life/Color.java
+++ src/main/java/com/example/conways_game_of_life/Color.java
@@ -3,6 +3,13 @@ package com.example.conways_game_of_life;
 public enum Color {
     // The following colors correspond to existing colors from the javafx.scene.paint.Class class.
     // We use the color WHITE for dead tiles.
-    YELLOW, ORANGE, RED, MAGENTA, PURPLE, VIOLET, BLUE, TEAL, GREEN, WHITE
+    WHITE, YELLOW, ORANGE, RED, MAGENTA, PURPLE, VIOLET, BLUE, TEAL, GREEN;
 
+    @Override
+    public String toString() {
+        if(this.ordinal() == 0){
+            return " ";
+        }
+        return Character.toString(this.name().charAt(0));
+    }
 }
