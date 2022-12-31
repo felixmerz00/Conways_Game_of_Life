2c5b37380d2d0abcadc77a0b45129b1466c49214
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2022-12-31 12:49:42 +0100

    tests for setPlayerColor and setPlayerName

--- src/test/java/com/example/conways_game_of_life/UserInteractionTest.java
+++ src/test/java/com/example/conways_game_of_life/UserInteractionTest.java
@@ -46,4 +46,98 @@ class UserInteractionTest {
         String actual = testUserInteraction.setPlayerName(1);
         assertEquals("Felix", actual);
     }
+
+    // Test setPlayerColor for correct input.
+    @Test
+    void testSetPlayerColor1() throws NoSuchFieldException, IllegalAccessException {
+        UserInteraction testUserInteraction;
+        GameLogic testGameLogic = new GameLogic();
+        Field uiField = GameLogic.class.getDeclaredField("ui");
+        uiField.setAccessible(true);
+        testUserInteraction = (UserInteraction) uiField.get(testGameLogic);
+
+        // Set input stream.
+        String input = "0";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        // Call UUT and make assertions.
+        Color actual = testUserInteraction.setPlayerColor(1);
+        assertEquals(Color.YELLOW, actual);
+    }
+
+    // Test setPlayerColor with input of an integer below zero.
+    @Test
+    void testSetPlayerColor2() throws NoSuchFieldException, IllegalAccessException {
+        UserInteraction testUserInteraction;
+        GameLogic testGameLogic = new GameLogic();
+        Field uiField = GameLogic.class.getDeclaredField("ui");
+        uiField.setAccessible(true);
+        testUserInteraction = (UserInteraction) uiField.get(testGameLogic);
+
+        // Set input stream.
+        String input = "-1\n8";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        // Call UUT and make assertions.
+        Color actual = testUserInteraction.setPlayerColor(1);
+        assertEquals(Color.GREEN, actual);
+    }
+
+    // Test setPlayerColor with input of an integer above eight.
+    @Test
+    void testSetPlayerColor3() throws NoSuchFieldException, IllegalAccessException {
+        UserInteraction testUserInteraction;
+        GameLogic testGameLogic = new GameLogic();
+        Field uiField = GameLogic.class.getDeclaredField("ui");
+        uiField.setAccessible(true);
+        testUserInteraction = (UserInteraction) uiField.get(testGameLogic);
+
+        // Set input stream.
+        String input = "9\n7";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        // Call UUT and make assertions.
+        Color actual = testUserInteraction.setPlayerColor(1);
+        assertEquals(Color.TEAL, actual);
+    }
+
+    // Test setPlayerColor with input of an integer which was already chosen by the first player.
+    @Test
+    void testSetPlayerColor4() throws NoSuchFieldException, IllegalAccessException {
+        UserInteraction testUserInteraction;
+        GameLogic testGameLogic = new GameLogic();
+        Field uiField = GameLogic.class.getDeclaredField("ui");
+        uiField.setAccessible(true);
+        testUserInteraction = (UserInteraction) uiField.get(testGameLogic);
+
+        // Set input for the first player.
+        String input = "7\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        testUserInteraction.setPlayerColor(1);
+        // Set the input for the second player.
+        input = "7\n6";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        // Call UUT and make assertions.
+        Color actual = testUserInteraction.setPlayerColor(2);
+        assertEquals(Color.BLUE, actual);
+    }
+
+    // Test setPlayerColor with input which is of the wrong type e.g. not integer.
+    @Test
+    void testSetPlayerColor5() throws NoSuchFieldException, IllegalAccessException {
+        UserInteraction testUserInteraction;
+        GameLogic testGameLogic = new GameLogic();
+        Field uiField = GameLogic.class.getDeclaredField("ui");
+        uiField.setAccessible(true);
+        testUserInteraction = (UserInteraction) uiField.get(testGameLogic);
+
+        // Set input stream.
+        String input = "ORANGE\n2";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        // Call UUT and make assertions.
+        Color actual = testUserInteraction.setPlayerColor(1);
+        assertEquals(Color.RED, actual);
+    }
 }
